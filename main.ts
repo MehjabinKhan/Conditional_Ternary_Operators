6c88b1b20287f3ddd0a8b5e6074b7f1989f507f0
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-22 23:29:03 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+let areEnjoying = "true";
+let rides = areEnjoying ? "Rollercoaster" : "Droptower";
+console.log(rides);
+
+let yourAge = 21;
+let b = yourAge >= 21 ? "you are allowed" : "you are not allowed";
+console.log(b);
+
+let bestMemorableDay = "false";
+let day = bestMemorableDay ? "first day of school" : "picnic at seaside";
+console.log(day);
+
+let teaTime = "true";
+let snacks = teaTime ? "biscuits" : "tea";
+console.log(snacks);
+
+let weather = "true";
+let hotDay = weather ? "stay hydrated" : "stay in door";
+console.log(hotDay);
